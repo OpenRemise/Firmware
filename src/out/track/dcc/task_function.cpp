f66ab3e413980a5d809a8d5ddecf293e6ba79cf7
Vincent Hamp <vincent.hamp@higaski.at>
2024-01-26 15:18:31 +0100

    Remove local copy of DCC encoder

--- src/out/track/dcc/task_function.cpp
+++ src/out/track/dcc/task_function.cpp
@@ -7,10 +7,10 @@
 #include "task_function.hpp"
 #include <driver/gpio.h>
 #include <dcc/dcc.hpp>
+#include <ztl/fail.hpp>
 #include <ztl/inplace_deque.hpp>
 #include "log.h"
 #include "resume.hpp"
-#include "rmt_dcc_encoder.h"
 
 namespace out::track::dcc {
 
@@ -22,12 +22,46 @@ namespace {
 bool d20_state{};  // ch2
 bool d21_state{};  // ch3
 
+struct Offsets {
+  uint8_t endbit{};
+  uint8_t tcs{};
+};
+
+consteval Offsets make_offsets() {
+  using namespace std::literals;
+  if constexpr (OPTIMIZATION == "-Og"sv)
+    return {
+      .endbit = 30u,
+      .tcs = 21u,
+    };
+  else if constexpr (OPTIMIZATION == "-Os"sv)
+    return {
+      .endbit = 0u,
+      .tcs = 0u,
+    };
+  else if constexpr (OPTIMIZATION == "-O2"sv)
+    return {
+      .endbit = 0u,
+      .tcs = 0u,
+    };
+  else if constexpr (OPTIMIZATION == "-O0"sv)
+    return {
+      .endbit = 0u,
+      .tcs = 0u,
+    };
+  else { ztl::fail(); }
+}
+
+auto const offsets{make_offsets()};
+
 /// TODO
 bool IRAM_ATTR rmt_callback(rmt_channel_handle_t,
                             rmt_tx_done_event_data_t const*,
                             void*) {
   gptimer_set_raw_count(gptimer, 0u);
-  gptimer_alarm_config_t const alarm_config{.alarm_count = Timing::BiDiTCSMin};
+  gptimer_alarm_config_t const alarm_config{
+    .alarm_count = static_cast<decltype(gptimer_alarm_config_t::alarm_count)>(
+      BiDiTCSMin + offsets.tcs)};
   gptimer_set_alarm_action(gptimer, &alarm_config);
 
   // TODO REMOVE
@@ -40,30 +74,32 @@ bool IRAM_ATTR rmt_callback(rmt_channel_handle_t,
 bool IRAM_ATTR gptimer_callback(gptimer_handle_t timer,
                                 gptimer_alarm_event_data_t const* edata,
                                 void*) {
-  // Start of channel 1
-  if (edata->alarm_value < Timing::BiDiTTS1) {
+  // TCS
+  if (edata->alarm_value < BiDiTTS1) {
+    gptimer_set_raw_count(timer, BiDiTCSMin);
+
     // Pull tracks low
     // TODO
+    gpio_set_level(d21_gpio_num, true);
 
-    // Reset alarm to end of channel 1
-    gptimer_alarm_config_t const alarm_config{.alarm_count = Timing::BiDiTTC1};
+    // Reset alarm to TS2
+    gptimer_alarm_config_t const alarm_config{.alarm_count = BiDiTTC1};
     gptimer_set_alarm_action(timer, &alarm_config);
   }
-  // End of channel 1
+  // TS2
   else if (edata->alarm_value < BiDiTTS2) {
-    // Reset alarm to end of cutout
-    gptimer_alarm_config_t const alarm_config{.alarm_count = Timing::BiDiTCE};
+    // Reset alarm to TCE
+    gptimer_alarm_config_t const alarm_config{.alarm_count = BiDiTCEMin};
     gptimer_set_alarm_action(timer, &alarm_config);
   }
-  // End of cutout
+  // TCE
   else {
     // Release tracks
     // TODO
+    gpio_set_level(d21_gpio_num, false);
 
     gptimer_set_alarm_action(gptimer, NULL);
   }
-
-  gpio_set_level(d20_gpio_num, d20_state = !d20_state);
 
   return pdFALSE;
 }
@@ -91,7 +127,7 @@ void operations_loop() {
     Packet packet{0b00001111u, 0b00111100u, 0b11110000u};
 
     ESP_ERROR_CHECK(transmit_packet(packet));
-    ESP_ERROR_CHECK(gpio_set_level(d21_gpio_num, d21_state = !d21_state));
+    // ESP_ERROR_CHECK(gpio_set_level(d21_gpio_num, d21_state = !d21_state));
 
     // RailCom stuff
     // Read notification value and so on?
@@ -110,14 +146,15 @@ void task_function(void*) {
 
     static_assert(OPTIMIZATION == std::string_view{"-Og"});
 
-    dcc_encoder_config_t encoder_config{.num_preamble = 17u,
-                                        .bit1_duration = 58u,
-                                        .bit0_duration = 100u,
-                                        .endbit_duration = 58u - 24u,
-                                        .flags{
-                                          .cutout = true,
-                                          // .zimo0 = true,
-                                        }};
+    dcc_encoder_config_t encoder_config{
+      .num_preamble = 17u,
+      .cutoutbit_duration = 60u,
+      .bit1_duration = 58u,
+      .bit0_duration = 100u,
+      .endbit_duration = static_cast<uint8_t>(58u - offsets.endbit),
+      .flags{
+        .zimo0 = true,
+      }};
 
     ESP_ERROR_CHECK(resume(encoder_config, rmt_callback, gptimer_callback));
 
