62eaa2920c58e33260350e85948945ccda3bd07b
Vincent Hamp <vincent.hamp@higaski.at>
2025-05-18 13:47:56 +0200

    build: update to DCC 0.41.0, Z21 0.3.1, ULF_DCC_EIN 0.5.0 and ULF_SUSIV2 0.2.0

--- src/usb/rx_task_function.cpp
+++ src/usb/rx_task_function.cpp
@@ -41,7 +41,7 @@ using namespace std::literals;
 void transmit_ping() {
 #if CONFIG_IDF_TARGET_ESP32S3
   auto const app_desc{esp_app_get_description()};
-  auto const ping{ulf::com::ping("OpenRemise", app_desc->version + 1, 'D')};
+  auto const ping{ulf::com::ping("OpenRemise", app_desc->version + 1)};
   xStreamBufferSend(tx_stream_buffer.handle, data(ping), size(ping), 0u);
 #endif
 }
