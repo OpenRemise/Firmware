38b3400ab367b01ff90914e66cf7d3dde15aaa75
Vincent Hamp <vincent.hamp@higaski.at>
2025-11-19 08:21:43 +0100

    fix: add DECUP ack manipulation workaround (#105)

--- src/drv/out/track/zimo/decup/task_function.cpp
+++ src/drv/out/track/zimo/decup/task_function.cpp
@@ -101,6 +101,53 @@ uint8_t receive_acks(uint32_t us) {
 }
 
 /// \todo document
+/// https://github.com/OpenRemise/Firmware/issues/105
+uint8_t manipulate_acks_workaround(Packet const& packet, uint8_t acks) {
+  static uint8_t decoder_id{};
+
+  // No ack can only be preamble
+  if (auto const count{size(packet)}; !acks) {
+    if (count == 1uz &&
+        (packet[0uz] == std::to_underlying(Command::Preamble0) ||
+         packet[0uz] == std::to_underlying(Command::Preamble1)))
+      decoder_id = 0u;
+  }
+  // Manipulate acks depending on mode
+  else
+    switch (state.load()) {
+      case State::DECUPZpp:
+        // Flash erase
+        if (count == 4uz && (packet[0uz] == 0x03u && packet[1uz] == 0x55u &&
+                             packet[2uz] == 0xFFu && packet[3uz] == 0xFFu))
+          acks = 1u;
+        // Flash write, CV write
+        else acks = 2u;
+        break;
+
+      case State::DECUPZsu:
+        // Decoder ID, block count and security bytes
+        if (count == 1uz) {
+          // Decoder ID
+          if (!decoder_id) {
+            decoder_id = packet[0uz];
+            acks = 2u;
+          }
+          // Block count and security bytes
+          else
+            acks = 1u;
+        }
+        // Flash write
+        else
+          acks = 2u;
+        break;
+
+      default: assert(false); break;
+    }
+
+  return acks;
+}
+
+/// \todo document
 esp_err_t transmit_acks(uint8_t acks) {
   return xMessageBufferSend(
            rx_message_buffer.handle, &acks, sizeof(acks), 0u) == sizeof(acks)
@@ -123,13 +170,14 @@ esp_err_t loop() {
     // Transmit packet
     else {
       if (get_current_limit() == CurrentLimit::_4100mA &&
-          packet->front() != std::to_underlying(Command::Preamble0) &&
-          packet->front() != std::to_underlying(Command::Preamble1))
+          (*packet)[0uz] != std::to_underlying(Command::Preamble0) &&
+          (*packet)[0uz] != std::to_underlying(Command::Preamble1))
         ESP_ERROR_CHECK(set_current_limit(CurrentLimit::_1300mA));
       ESP_ERROR_CHECK(transmit_packet_blocking(*packet));
       auto const us{packet2timeout(*packet)};
       auto const acks{receive_acks(us)};
-      ESP_ERROR_CHECK(transmit_acks(acks));
+      auto const manipulated_acks{manipulate_acks_workaround(*packet, acks)};
+      ESP_ERROR_CHECK(transmit_acks(manipulated_acks));
     }
   }
 }
@@ -150,7 +198,8 @@ esp_err_t test_loop(uint8_t decoder_id = 221u) {
   Packet packet{decoder_id};
   ESP_ERROR_CHECK(transmit_packet_blocking(packet));
   auto const acks{receive_acks(size(packet) > 1uz ? 100'000u : 5000u)};
-  if (acks == 2uz) {
+  auto const manipulated_acks{manipulate_acks_workaround(packet, acks)};
+  if (manipulated_acks == 2uz) {
     LOGI("DECUP test success");
     return ESP_OK;
   } else {
