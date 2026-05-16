7a552dbc7a3e230b2d4777adb780e0029b49ce20
Vincent Hamp <vincent.hamp@higaski.at>
2026-05-16 15:28:07 +0200

    enable display

--- src/mw/disp/init.cpp
+++ src/mw/disp/init.cpp
@@ -35,24 +35,22 @@ namespace mw::disp {
 /// bytes each), assigns default UART0 TX/RX pins, and starts the display \ref
 /// task running \ref task_function().
 esp_err_t init() {
-  // static constexpr uart_config_t uart_config{.baud_rate =
-  // CONFIG_MONITOR_BAUD,
-  //                                            .data_bits = UART_DATA_8_BITS,
-  //                                            .parity = UART_PARITY_DISABLE,
-  //                                            .stop_bits = UART_STOP_BITS_1,
-  //                                            .flow_ctrl =
-  //                                              UART_HW_FLOWCTRL_DISABLE,
-  //                                            .source_clk =
-  //                                            UART_SCLK_DEFAULT};
-  // ESP_ERROR_CHECK(uart_driver_install(UART_NUM_0, 1024, 1024, 0, NULL, 0));
-  // ESP_ERROR_CHECK(uart_param_config(UART_NUM_0, &uart_config));
-  // ESP_ERROR_CHECK(uart_set_pin(UART_NUM_0,
-  //                              U0TXD_GPIO_NUM,
-  //                              U0RXD_GPIO_NUM,
-  //                              UART_PIN_NO_CHANGE,
-  //                              UART_PIN_NO_CHANGE));
+  static constexpr uart_config_t uart_config{.baud_rate = CONFIG_MONITOR_BAUD,
+                                             .data_bits = UART_DATA_8_BITS,
+                                             .parity = UART_PARITY_DISABLE,
+                                             .stop_bits = UART_STOP_BITS_1,
+                                             .flow_ctrl =
+                                               UART_HW_FLOWCTRL_DISABLE,
+                                             .source_clk = UART_SCLK_DEFAULT};
+  ESP_ERROR_CHECK(uart_driver_install(UART_NUM_0, 1024, 1024, 0, NULL, 0));
+  ESP_ERROR_CHECK(uart_param_config(UART_NUM_0, &uart_config));
+  ESP_ERROR_CHECK(uart_set_pin(UART_NUM_0,
+                               U0TXD_GPIO_NUM,
+                               U0RXD_GPIO_NUM,
+                               UART_PIN_NO_CHANGE,
+                               UART_PIN_NO_CHANGE));
 
-  // task.create(task_function);
+  task.create(task_function);
 
   return ESP_OK;
 }
